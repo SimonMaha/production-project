57d593761cdcf1a39e49be3162dfebb0c10283df
@smaha1 <smaha@cloverdynamics.com>
2024-02-27 13:02:11 +0200

    fix import

--- src/entity/Counter/model/slice/counterSlice.ts
+++ src/entity/Counter/model/slice/counterSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { CounterSchema } from '../types/CounterSchema';
+import { CounterSchema } from 'entity/Counter';
 
 const initialState: CounterSchema = {
   value: 0
