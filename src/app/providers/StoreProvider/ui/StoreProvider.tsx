761de7fa4e42fc5dbf222d9e8f44a9c7aef2be24
@smaha1 <smaha@cloverdynamics.com>
2024-02-28 13:36:47 +0200

    fix store provide and store decorator

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -3,19 +3,25 @@ import { Provider } from 'react-redux';
 import { createReduxStore } from '../config/store';
 import { StateScheme } from '../config/StateSchema';
 import { DeepPartial } from 'ts-essentials';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
   children?: ReactNode;
   initialState?: DeepPartial<StateScheme>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateScheme>>;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
   const {
     children,
     initialState,
+    asyncReducers,
   } = props;
 
-  const store = createReduxStore(initialState as StateScheme);
+  const store = createReduxStore(
+    initialState as StateScheme,
+    asyncReducers as ReducersMapObject<StateScheme>,
+  );
 
   return (
     <Provider store={store}>
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,19 @@
 import { StoryFn } from '@storybook/react';
 import { StateScheme, StoreProvider } from 'app/providers/StoreProvider';
 import { DeepPartial } from 'ts-essentials';
+import { ReducersMapObject } from '@reduxjs/toolkit';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 
-export const StoreDecorator = (state: DeepPartial<StateScheme>) => (Story: StoryFn) => (
-  <StoreProvider initialState={state}>
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateScheme>> = {
+  loginForm: loginReducer,
+};
+
+export const StoreDecorator = (
+  state: DeepPartial<StateScheme>,
+  asyncReducers?: DeepPartial<ReducersMapObject<StateScheme>>
+) => (Story: StoryFn) => (
+  <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
     <Story />
   </StoreProvider>
 );
+ 
